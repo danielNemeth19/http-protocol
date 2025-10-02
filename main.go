5a61b1415bae3a7e41c3c5a73f53f3f9b18fc1da
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-10-02 23:54:51 +0200

    (feat) Works on buffered printing of file

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Printf("I hope I get the job!")
+	fh, err := os.Open("messages.txt")
+	if err != nil {
+		panic("Problem opening the file")
+	}
+
+	defer fh.Close()
+	scanner := bufio.NewScanner(fh)
+	buf := make([]byte, 4096)
+	scanner.Buffer(buf, 8)
+	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		if len(data) == 8 {
+			return 8, data[:8], nil
+		}
+		if atEOF && len(data) > 0 {
+			return len(data), data, nil
+		}
+		return 0, nil, nil
+	})
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 }
