c5479a086e6e3db879081f7df88b5307bf18504f
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-10-03 23:58:24 +0200

    (feat) Fixes split function solution with scanner; works on simple buffer

--- main.go
+++ main.go
@@ -6,18 +6,12 @@ import (
 	"os"
 )
 
-func main() {
-	fh, err := os.Open("messages.txt")
-	if err != nil {
-		panic("Problem opening the file")
-	}
-
-	defer fh.Close()
-	scanner := bufio.NewScanner(fh)
-	buf := make([]byte, 4096)
+func withScanner(f *os.File) {
+	scanner := bufio.NewScanner(f)
+	buf := make([]byte, 8)
 	scanner.Buffer(buf, 8)
 	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		if len(data) == 8 {
+		if len(data) >= 8 {
 			return 8, data[:8], nil
 		}
 		if atEOF && len(data) > 0 {
@@ -27,6 +21,26 @@ func main() {
 	})
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func main() {
+	fh, err := os.Open("messages.txt")
+	if err != nil {
+		panic("Problem opening the file")
+	}
+
+	defer fh.Close()
+	// withScanner(fh)
+	buf := make([]byte, 8)
+	var start int64
+	for {
+		_, err := fh.ReadAt(buf, start)
+		if err != nil {
+			break
+		}
+		fmt.Printf("read: %s\n", buf)
+		start = start + 8
 	}
 }
