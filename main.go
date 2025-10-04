199a7958f60ee7a7645af49b59d4dd667ab48489
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-10-04 20:49:07 +0200

    (feat) Prints buffered text when endline is reached

--- main.go
+++ main.go
@@ -1,46 +1,36 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
-
-func withScanner(f *os.File) {
-	scanner := bufio.NewScanner(f)
-	buf := make([]byte, 8)
-	scanner.Buffer(buf, 8)
-	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		if len(data) >= 8 {
-			return 8, data[:8], nil
-		}
-		if atEOF && len(data) > 0 {
-			return len(data), data, nil
-		}
-		return 0, nil, nil
-	})
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Printf("read: %s\n", line)
-	}
-}
 
 func main() {
 	fh, err := os.Open("messages.txt")
 	if err != nil {
 		panic("Problem opening the file")
 	}
-
 	defer fh.Close()
-	// withScanner(fh)
+
 	buf := make([]byte, 8)
 	var start int64
+	curr_line := ""
 	for {
-		_, err := fh.ReadAt(buf, start)
+		n, err := fh.ReadAt(buf, start)
+		if n > 0 {
+			parts := strings.Split(string(buf[:n]), "\n")
+			if len(parts) == 1 {
+				curr_line += parts[0]
+			} else if len(parts) == 2 {
+				curr_line += parts[0]
+				fmt.Printf("read: %s\n", curr_line)
+				curr_line = parts[1]
+			}
+			start += int64(n)
+		}
 		if err != nil {
 			break
 		}
-		fmt.Printf("read: %s\n", buf)
-		start = start + 8
 	}
 }
