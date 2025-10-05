ce3317e9d27952c36d9518eae130a9ec4fbf48d8
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-10-05 22:59:15 +0200

    (feat) Implements buffered read using channels

--- main.go
+++ main.go
@@ -2,35 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	buf := make([]byte, 8)
+	curr_line := ""
+	go func() {
+		defer f.Close()
+		for {
+			n, err := f.Read(buf)
+			if n > 0 {
+				parts := strings.Split(string(buf[:n]), "\n")
+				if len(parts) == 1 {
+					curr_line += parts[0]
+				} else if len(parts) == 2 {
+					curr_line += parts[0]
+					ch <- curr_line
+					curr_line = parts[1]
+				}
+			}
+			if err != nil {
+				break
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
 
 func main() {
 	fh, err := os.Open("messages.txt")
 	if err != nil {
 		panic("Problem opening the file")
 	}
-	defer fh.Close()
-
-	buf := make([]byte, 8)
-	var start int64
-	curr_line := ""
-	for {
-		n, err := fh.ReadAt(buf, start)
-		if n > 0 {
-			parts := strings.Split(string(buf[:n]), "\n")
-			if len(parts) == 1 {
-				curr_line += parts[0]
-			} else if len(parts) == 2 {
-				curr_line += parts[0]
-				fmt.Printf("read: %s\n", curr_line)
-				curr_line = parts[1]
-			}
-			start += int64(n)
-		}
-		if err != nil {
-			break
-		}
+	for c := range getLinesChannel(fh) {
+		fmt.Printf("read: %s\n", c)
 	}
 }
