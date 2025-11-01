017f5d34ae73e916d91cadbae8ad34701a7ad8b5
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-11-02 00:00:21 +0100

    (feat) Completes header parsing

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -9,24 +9,31 @@ var endLine = []byte("\r\n")
 
 type Headers map[string]string
 
+func NewHeaders() Headers {
+	header := make(Headers)
+	return header
+}
+
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	if bytes.HasPrefix(data, endLine) {
 		return len(endLine), true, nil
 	}
-	i := bytes.Index(data, endLine); if i == -1 {
+	i := bytes.Index(data, endLine)
+	if i == -1 {
 		return 0, false, nil
 	}
+
 	fieldLine := data[:i]
-	fieldSep := bytes.IndexByte(fieldLine, ':'); if fieldSep == -1 {
-		return 0, false, fmt.Errorf("Field line supposed to a ':' separator")
+	fieldSep := bytes.IndexByte(fieldLine, ':')
+	if fieldSep == -1 {
+		return 0, false, fmt.Errorf("Field line supposed to have a ':' separator")
 	}
 
-
-	// parts := strings.Split(fieldLine[0], ":")
-	// if len(parts) != 2 {
-		// return 0, false, fmt.Errorf("Field line supposed to have two parts, got: %d\n", len(parts))
-	// }
-	// fieldName, fieldValue := parts[0], parts[1]
-	// fmt.Println(fieldName, fieldValue)
-	return 0, false, nil
+	fieldName := bytes.TrimLeft(fieldLine[:fieldSep], " ")
+	fieldValue := bytes.Trim(fieldLine[fieldSep+1:], " ")
+	if bytes.HasSuffix(fieldName, []byte(" ")) {
+		return 0, false, fmt.Errorf("Invalid field name")
+	}
+	h[string(fieldName)] = string(fieldValue)
+	return len(fieldLine) + len(endLine), false, nil
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -1,16 +1,11 @@
 package headers
 
 import (
-	// "io"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func NewHeaders() Headers {
-	var header Headers
-	return header
-}
 
 func TestValidSingleHeader(t *testing.T) {
 	headers := NewHeaders()
@@ -23,11 +18,68 @@ func TestValidSingleHeader(t *testing.T) {
 	assert.False(t, done)
 }
 
-// func TestInvalidSpacingHeader(t *testing.T) {
-	// headers := NewHeaders()
-	// data := []byte("       Host : localhost:42069       \r\n\r\n")
-	// n, done, err := headers.Parse(data)
-	// require.Error(t, err)
-	// assert.Equal(t, 0, n)
-	// assert.False(t, done)
-// }
+func TestValidSingleHeaderWithExtraSpace(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("  Host:localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 24, n)
+	assert.False(t, done)
+}
+
+func TestValidTwoHeadersWithExistingHeaders(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host:localhost:42069\r\n")
+	headers.Parse(data)
+	data = []byte("Content-Type: application/json\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "application/json", headers["Content-Type"])
+	assert.Equal(t, 32, n)
+	assert.False(t, done)
+}
+
+func TestNotEnoughData(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Content-Type: application")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.False(t, done)
+	assert.Equal(t, 0, n)
+}
+
+func TestInvalidFieldLine(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("InvalidFieldLine\r\n")
+	n, done, err := headers.Parse(data)
+	require.Error(t, err)
+	require.EqualError(t, err, "Field line supposed to have a ':' separator")
+	require.NotNil(t, headers)
+	assert.False(t, done)
+	assert.Equal(t, 0, n)
+}
+
+func TestValidDone(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.True(t, done)
+	assert.Equal(t, 2, n)
+}
+
+func TestInvalidSpacingHeader(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.Error(t, err)
+	require.EqualError(t, err, "Invalid field name")
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
